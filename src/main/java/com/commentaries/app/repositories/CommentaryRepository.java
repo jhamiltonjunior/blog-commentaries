79ad091f987c145fde9ee3d167cedde39073512d
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-22 20:40:26 -0300

    java

--- src/main/java/com/commentaries/app/repositories/CommentaryRepository.java
+++ src/main/java/com/commentaries/app/repositories/CommentaryRepository.java
@@ -3,6 +3,6 @@ package com.commentaries.app.repositories;
 import com.commentaries.domain.entities.interfaces.CommentaryData;
 
 public interface CommentaryRepository {
-  void deleteById(String userId, String commentaryId);
+  String deleteById(String userId, String commentaryId);
   void add(CommentaryData commentary, String userId, String articleId);
 }
