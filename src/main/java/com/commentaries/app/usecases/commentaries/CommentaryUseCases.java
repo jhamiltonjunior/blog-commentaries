a8962e85c6b2249b3d6aee46752570fe39a717a4
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2022-07-23 20:59:48 -0300

    feature: createCommentaryOnDatabase and deleteCommentary method added in CommentaryUseCases

--- src/main/java/com/commentaries/app/usecases/commentaries/CommentaryUseCases.java
+++ src/main/java/com/commentaries/app/usecases/commentaries/CommentaryUseCases.java
@@ -1,5 +1,25 @@
 package com.commentaries.app.usecases.commentaries;
 
-public class CommentaryUseCases {
-  
+import com.commentaries.app.usecases.commentaries.interfaces.CommentaryInterface;
+import com.commentaries.domain.entities.Commentary;
+import com.commentaries.domain.entities.interfaces.AuthorData;
+import com.commentaries.domain.entities.interfaces.CommentaryData;
+import com.commentaries.shared.Either;
+
+public class CommentaryUseCases implements CommentaryInterface {
+  public Either<Error, CommentaryData> createCommentaryOnDatabase(CommentaryData commentary, AuthorData author) {
+    Either<Error, Commentary> commentaryOrError = Commentary.create(commentary);
+
+    if (commentaryOrError.isLeft()) {
+      return Either.left(new Error("Does not posible create commentary"));
+    }
+
+    Commentary commentaryData = commentaryOrError.Right().get();
+
+    return Either.left(new Error());
+  }
+
+  public Either<Error, String> deleteCommentary (String userId, String commentaryId) {
+    return Either.right("i");
+  }
 }
